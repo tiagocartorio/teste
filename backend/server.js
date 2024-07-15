20c3a1cc0bbcdcce7483cb4fe58a331211b9dd86
Tiago <cartoriocampo@gmail.com>
2024-07-15 17:17:33 -0300

    Envio de arquivos para o servidor

--- backend/server.js
+++ backend/server.js
@@ -4,34 +4,29 @@ const compression = require('compression');
 const sql = require('./modules/db.js');
 const PORT = 3000;
 const app = express();
-
 app.use(compression());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 const obterDadosPorId = async (id) => {
   try {
-    const res = await sql`SELECT * FROM dados WHERE id = ${id}`;
-    return res[0];
+    const res = await sql`SELECT * FROM dados WHERE id = ${id}`
+    return res[0]
   } catch (err) {
-    return null;
+    return null
   }
 };
-
 const inserirDados = async (username, password) => {
   try {
-    await sql`INSERT INTO users (username, password) VALUES (${username}, ${password})`;
-    return true;
+    await sql`INSERT INTO users (username, password) VALUES (${username}, ${password})`
+    return true
   } catch (err) {
-    console.error('Erro ao inserir dados:', err);
+    console.error('Erro ao inserir dados:', err)
     return false;
   }
-};
-
-app.use(express.static(path.join(__dirname, '../public')));
-
+}
+app.use(express.static(path.join(__dirname, '../public')))
 app.get('/:id', async (req, res) => {
   const id = req.params.id;
   const pagina = await obterDadosPorId(id);
@@ -40,11 +35,9 @@ app.get('/:id', async (req, res) => {
   } else {
     res.render('erro', { url: req.url });
   }
-});
-
+})
 app.post('/teste', async (req, res) => {
-  const { username, password } = req.body;
-
+  const { username, password } = req.body
   if (username === password) {
     const sucesso = await inserirDados(username, password);
     if (sucesso) {
@@ -55,12 +48,10 @@ app.post('/teste', async (req, res) => {
   } else {
     res.send("Erro, senhas incorretas");
   }
-});
-
+})
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/', 'index.html'));
-});
-
+})
 app.listen(PORT, () => {
   console.log(`Servidor está rodando em http://localhost:${PORT}`);
-});
+})
